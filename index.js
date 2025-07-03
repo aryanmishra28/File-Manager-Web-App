840c86be57fd2c1c9b221be64284d5e6ef64de05
aryanmishra28 <aryanmishra1851@gmail.com>
2025-07-03 22:42:44 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app=express();
-const path = require("path"); //: Helps in handling file paths across OS.
+const path = require("path"); // Helps in handling file paths across OS.
 const fs= require("fs"); //Node’s built-in File System module for file operations.
 
 
